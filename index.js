d5acadbf9a3594a13ab9ece1e97dc6a07fe38205
Igor Ilic <github@igorilic.net>
2021-12-15 22:58:19 +0100

    Add arguments input and pass them to memebox on trigger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Extension, PLATFORMS, INPUT_METHOD} = require("deckboard-kit");
 const axios = require("axios");
 const WebSocket = require("ws");
+const jsonic = require("jsonic");
 
 class MemeBox extends Extension {
 	constructor(props) {
@@ -36,12 +37,13 @@ class MemeBox extends Extension {
 				color: "#6441a4",
 				input: [
 					{
-						label: "Media to trigger",
+						label: "Action to trigger",
 						ref: "mediaID",
 						type: "input:autocomplete",
 					},
 					{
 						label: "Arguments (Optional)",
+						value: "test123",
 						ref: "memeboxArgs",
 						type: INPUT_METHOD.INPUT_TEXTAREA,
 					},
@@ -67,14 +69,14 @@ class MemeBox extends Extension {
 	getAutocompleteOptions(ref) {
 		switch (ref) {
 			case "mediaID":
-				return this.getMemeboxMedia();
+				return this.getMemeboxAction();
 			default:
 				return [];
 		}
 
 	}
 
-	async getMemeboxMedia() {
+	async getMemeboxAction() {
 		try {
 			const {memeboxAddress, memeboxPort} = this.configs;
 
@@ -91,10 +93,11 @@ class MemeBox extends Extension {
 			const data = result.data;
 			//if (!data) return [];
 
-			return data.map((media) => {
+			return data.map((action) => {
+				const actionType = this._convertActionTypeToLabel(action.type);
 				return {
-					value: media.id,
-					label: media.name,
+					value: action.id,
+					label: `${actionType} - ${action.name}`,
 				};
 			});
 		} catch (e) {
@@ -105,11 +108,24 @@ class MemeBox extends Extension {
 		}
 	}
 
-	triggerMemeboxMedia(mediaID, memeboxArgs) {
+	triggerMemeboxAction(mediaID, memeboxArgs) {
+		let variables;
+
+		try {
+			variables = memeboxArgs ? jsonic(memeboxArgs) : null;
+		} catch (_) {
+			variables = null;
+		}
+
 		const triggerObj = {
 			id: mediaID,
 			repeatX: 0,
 			repeatSecond: 0,
+			overrides: {
+				action: {
+					variables
+				}
+			}
 		};
 		this.sendMemeboxTrigger(triggerObj);
 	}
@@ -120,7 +136,6 @@ class MemeBox extends Extension {
 		}
 	}
 
-
 	execute(action, params) {
 		switch (action) {
 			case "memebox-trigger":
@@ -129,14 +144,39 @@ class MemeBox extends Extension {
 					const {memeboxAddress, memeboxPort} = this.configs;
 					this.memeboxWS = new WebSocket(`ws://${memeboxAddress.value}:${memeboxPort.value}`);
 					this.memeboxWS.on("open", () => {
-						this.triggerMemeboxMedia(mediaID, memeboxArgs);
+						this.triggerMemeboxAction(mediaID, memeboxArgs);
 					});
 				} else {
-					this.triggerMemeboxMedia(mediaID, memeboxArgs);
+					this.triggerMemeboxAction(mediaID, memeboxArgs);
 				}
 				break;
 		}
 	};
+
+	_convertActionTypeToLabel(actionType) {
+		switch (parseInt(actionType)) {
+			case -1:
+				return "Invalid";
+			case 0:
+				return "Picture";
+			case 1:
+				return "Audio";
+			case 2:
+				return "Video";
+			case 3:
+				return "IFrame";
+			case 4:
+				return "Widget";
+			case 5:
+				return "Script";
+			case 98:
+				return "PermanentScript";
+			case 99:
+				return "WidgetTemplate";
+			case 100:
+				return "Meta";
+		}
+	}
 }
 
 module.exports = (sendData) => new MemeBox(sendData);
